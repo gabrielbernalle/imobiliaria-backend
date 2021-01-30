346df9e0c138bc6756b2ad9df757013c7aff8408
Cristiano <neivacristiano@yahoo.com.br>
2021-01-30 14:42:57 -0300

    feat: add attributes on propertie and relates to a owner

--- src/database/entities/Propertie.ts
+++ src/database/entities/Propertie.ts
@@ -1,5 +1,14 @@
-import { Entity, PrimaryGeneratedColumn, OneToOne } from 'typeorm';
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    OneToOne,
+    Column,
+    JoinColumn,
+    ManyToOne,
+} from 'typeorm';
+import { ColumnNumericTransformer } from '../transformers';
 import { Address } from './Address';
+import { Person } from './Person';
 
 @Entity('properties')
 export class Propertie {
@@ -10,4 +19,49 @@ export class Propertie {
         cascade: true,
     })
     adress: Address;
+
+    @ManyToOne(() => Person, person => person.id, {
+        cascade: ['remove'],
+    })
+    @JoinColumn({ name: 'ownerId' })
+    owner: Person;
+
+    @Column('uuid', { nullable: false })
+    ownerId: string;
+
+    @Column({ nullable: false })
+    type: 'casa' | 'apartamento';
+
+    @Column({ nullable: false, type: 'int' })
+    roomsAmount: number;
+
+    @Column({ nullable: false, type: 'int', default: 0 })
+    suitesAmount: number;
+
+    @Column({ nullable: false, type: 'int' })
+    livingRoomsAmount: number;
+
+    @Column({ nullable: false, type: 'int' })
+    diningRoomsAmount: number;
+
+    @Column({ nullable: false, type: 'int', default: 0 })
+    parkingAmount: number;
+
+    @Column({ nullable: false, type: 'int', default: 0 })
+    builtInCabinetsAmount: number;
+
+    @Column({ type: 'int', nullable: true })
+    floor?: number;
+
+    @Column('numeric', {
+        transformer: new ColumnNumericTransformer(),
+        nullable: true,
+    })
+    condominiumValue?: number;
+
+    @Column({ nullable: true })
+    fullConcierge?: boolean;
+
+    @Column({ nullable: true })
+    description?: string;
 }
